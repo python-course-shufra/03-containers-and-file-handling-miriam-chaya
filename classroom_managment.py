5e107de9d69a6cd60dcc7a2a6b59d46d3b7b2fef
miriam-chaya <ch63893@gmail.com>
2024-01-05 01:19:42 +0200

    3

--- classroom_managment.py
+++ classroom_managment.py
@@ -63,10 +63,16 @@ def add_grade(name, profession, grade):
 
 
 def avg_grade(name, profession):
-    """Returns the average of grades of the student
-    in the specified profession
-    """
-    pass
+    def avg_grade(name, profession):
+        for student in classroom:
+            if student['name'] == name:
+                grades = [grade for subj, grade in student['grades'] if subj == profession]
+                if len(grades) > 0:
+                    gpa = sum(grades) / len(grades)
+                    return gpa
+                else:
+                    return None
+        return None
 
 
 def get_professions(name):
