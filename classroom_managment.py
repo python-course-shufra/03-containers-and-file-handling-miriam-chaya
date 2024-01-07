e21ccd1c4a75a9207c1727564d650ea1b5b42177
miriam-chaya <ch63893@gmail.com>
2024-01-07 12:32:22 +0200

    ok

--- classroom_managment.py
+++ classroom_managment.py
@@ -41,12 +41,13 @@ def add_student(name, email=None):
             lowName+=s.lower()
         email=lowName+'@example.com'
     classroom.append({'name':name,'email':email,'grades':[]})
-
+    return
 
 def delete_student(name):
     for student in classroom:
         if student['name']==name:
             student.clear()
+        return 
 
 
 
@@ -54,19 +55,42 @@ def set_email(name, email):
     for student in classroom:
         if student['name']==name:
             student['email']=email
+        return 
 
 
 def add_grade(name, profession, grade):
     for student in classroom:
         if student['name']==name:
             student['grades']+=((profession,grade),)
+        return 
 
 
 
 def avg_grade(name, profession):
-    pass
+    count=0
+    sum=0
+    for student in classroom:
+        if(student['name']==name):
+            for sub,grade in student['grades']:
+                if (sub==profession):
+                    count+=1
+                    sum+=grade
+            return sum/count
+
+                
 
 
 def get_professions(name):
-    """Returns a list of unique professions that student has grades in"""
-    pass
+    professions=[]
+    for student in classroom:
+        if(student['name']==name):
+            professions=[sub for sub,grade in student['grades']]
+        for p in professions:
+            for sub in professions:
+                if(professions[p]==professions[sub]&p!=sub):
+                    professions[sub].clear()
+        return professions
+    return None
+
+
+            
